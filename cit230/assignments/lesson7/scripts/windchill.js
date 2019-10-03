98cd8313b3d432a9ec97ac3b4f015f05fd3bfa2a
Erik Martinez <ejm20658@gmail.com>
2019-10-03 14:23:44 -0600

    HUGE CLEAN UP AND CHANGES***

--- cit230/assignments/lesson7/scripts/windchill.js
+++
@@ -1,6 +0,0 @@
-var t = 73;
-var s = 10;
-var f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-document.getElementById("windchill").innerHTML = f.toFixed(2);
-document.getElementById("currenttemp").innerHTML = t;
-document.getElementById("currentspeed").innerHTML = s;
