1024bc900e3282a16a831e3863f740a33f1b48cf
Erik Martinez <ejm20658@gmail.com>
2019-10-15 09:16:52 -0600

    ;)

---
+++ selectedprojects/angular-gmap/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements AfterViewInit {
+  title = 'angular-gmap';
+  @ViewChild('mapContainer', { static: false })
+  gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 43.826069;
+  lng = -111.789528;
+
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 8
+  };
+
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map
+  });
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.marker.setMap(this.map);
+  }
+}
