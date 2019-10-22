dfc8eb4a801e36bb404d351d0411217c0a783f94
Erik Martinez <ejm20658@gmail.com>
2019-10-22 08:37:26 -0600

    ;)

---
+++ selectedprojects/angular_calender/calender/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular'; // for FullCalendar!
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FullCalendarModule,
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
