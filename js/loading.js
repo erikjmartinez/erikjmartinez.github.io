b77ec938d1ffb6784e6b48ab40d88cf01ee0b09f
Erik Martinez <ejm20658@gmail.com>
2019-12-17 21:59:39 -0500

    lots and lots of work. Almost there

--- js/loading.js
+++ js/loading.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", event => {
   setTimeout(function() {
     document.querySelector(".loader").classList.add("hide");
-    document.querySelector(".main__content").classList.remove("hide");
+    document.querySelector(".main-content").classList.remove("hide");
   }, 2300);
 });
