eb02908a220a0ab5adcdef29b893c04f445b0a76
Erik Martinez <ejm20658@gmail.com>
2020-04-21 13:52:30 -0400

    more features

--- selectedprojects/todo-deved/app.js
+++ selectedprojects/todo-deved/app.js
@@ -3,31 +3,36 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
 
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
-
+filterOption.addEventListener('click', filterTodo);
 // Functions
 
 function addTodo(event) {
   // Prevent from from submitting
   event.preventDefault();
 
-  // Todo div
+  // Create Todo div
   const todoDiv = document.createElement('div');
   todoDiv.classList.add('todo');
 
-  // Todo list
+  // Create Todo list
   const newTodo = document.createElement('li');
   newTodo.innerText = todoInput.value;
   newTodo.classList.add('todo-item');
 
-  // append
+  // Append
   todoDiv.appendChild(newTodo);
 
-  // buttons
+  // Save todos
+  saveLocalTodos(todoInput.value);
+
+  // Create buttons
   const completeButton = document.createElement('button');
   completeButton.innerHTML = '<i class="fas fa-check"></i>';
   completeButton.classList.add('complete-btn');
@@ -36,6 +41,8 @@ function addTodo(event) {
   const trashButton = document.createElement('button');
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
   trashButton.classList.add('trash-btn');
+
+  // Append
   todoDiv.appendChild(trashButton);
 
   // Append to list
@@ -49,6 +56,90 @@ function deleteCheck(e) {
   const item = e.target;
   if (item.classList[0] === 'trash-btn') {
     const todo = item.parentElement;
-    todo.remove();
+    // Animation
+    todo.classList.add('fall');
+    todo.addEventListener('transitionend', function () {
+      todo.remove();
+    });
   }
+
+  if (item.classList[0] === 'complete-btn') {
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'uncompleted':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+    }
+  });
+}
+
+function saveLocalTodos(todo) {
+  // Check
+  let todos;
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+  todos.push(todo);
+  localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+function getTodos() {
+  // Check
+  let todos;
+  if (localStorage.getItem('todos') === null) {
+    todos = [];
+  } else {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+  todos.forEach(function (todo) {
+    // Create Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    // Create Todo list
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todo;
+    newTodo.classList.add('todo-item');
+
+    // Append
+    todoDiv.appendChild(newTodo);
+
+    // Create buttons
+    const completeButton = document.createElement('button');
+    completeButton.innerHTML = '<i class="fas fa-check"></i>';
+    completeButton.classList.add('complete-btn');
+    todoDiv.appendChild(completeButton);
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+
+    // Append
+    todoDiv.appendChild(trashButton);
+
+    // Append to list
+    todoList.appendChild(todoDiv);
+  });
 }
