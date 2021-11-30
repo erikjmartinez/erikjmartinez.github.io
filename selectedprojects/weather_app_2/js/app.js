a2cd9055524c65fa260905187473b904903cb1ee
EJM <ejm20658@gmail.com>
2021-11-29 17:19:00 -0700

    Updated DarkSkys API to Visiual Crossing Weather

--- selectedprojects/weather_app_2/js/app.js
+++ selectedprojects/weather_app_2/js/app.js
@@ -6,7 +6,7 @@ window.addEventListener("load", () => {
 
 	getLocation().then((location) => {
 		console.log(location);
-		getWeather(location.lat, location.lon).then((data) => {
+		getWeather(location.city, location.region).then((data) => {
 			console.log(data);
 
 			// Remove preloader
@@ -18,17 +18,17 @@ window.addEventListener("load", () => {
 			showContent.classList.remove("hide");
 
 			// Values from API
-			const { icon, temperature, summary } = data.currently;
+			const { icon, temp, conditions } = data.currentConditions;
 
 			// Math for converting fahrenheit to celsius
-			let celsius = (temperature - 32) * (5 / 9);
-			var degree = temperature + "\u00B0" + "F";
+			let celsius = (temp - 32) * (5 / 9);
+			var degree = temp + "\u00B0" + "F";
 			// + celsius.toFixed(1) + "\u00B0" + "C";
 
 			// Set value and display to DOM
 			document.getElementById("timezone").innerHTML =
 				location.city + ", " + location.region;
-			document.querySelector(".c-weather__summary").innerHTML = summary;
+			document.querySelector(".c-weather__summary").innerHTML = conditions;
 			document.getElementById("c-temp").innerHTML = degree;
 
 			const weathericon = document.querySelector(".c-weather__icon");
@@ -37,25 +37,27 @@ window.addEventListener("load", () => {
 			//Set Icon
 			setIcons(icon, document.querySelector(".icon"));
 
-			// Generate 5 day forecast
-			// let dailyData = data.daily.data;
-			// const forecastList = document.querySelector(".c-forecast ol");
-			// for (var i = 0; i < 5; i++) {
-			// 	let iconId = `c-forecast--icon--${i}`;
-			// 	const item = document.createElement("li");
+			// // Generate 5 day forecast
+			let dailyData = data.days;
+			const forecastList = document.querySelector(".c-forecast ol");
+			for (var i = 1; i <= 5; i++) {
+				let iconId = `c-forecast--icon--${i}`;
+				const item = document.createElement("li");
 
-			// 	item.classList.add("c-forecast__card");
-			// 	item.innerHTML =
-			// 		"<div class='c-weather__info'><canvas id=" +
-			// 		iconId +
-			// 		" width='128' height='128'></canvas><p class='c-weather__summary'>" +
-			// 		dailyData[i].summary +
-			// 		"</p><p>" +
-			// 		dailyData[i].temperatureHigh +
-			// 		"&deg;F</p></div>";
-			// 	forecastList.appendChild(item);
-			// 	setIcons(dailyData[i].icon, document.getElementById(iconId));
-			// }
+				item.classList.add("c-forecast__card");
+				item.innerHTML =
+					"<div class='c-weather__info'><canvas id=" +
+					iconId +
+					" width='128' height='128'></canvas><p class='c-weather__summary'>" +
+					dailyData[i].conditions +
+					"</p><p>" +
+					dailyData[i].temp +
+					"&deg;F</p><p>" +
+					dailyData[i].datetime +
+					"</p></div>";
+				forecastList.appendChild(item);
+				setIcons(dailyData[i].icon, document.getElementById(iconId));
+			}
 		});
 	});
 });
@@ -69,10 +71,10 @@ function getLocation() {
 	});
 }
 
-function getWeather(lat, long) {
-	const proxy = "https://cors-anywhere.herokuapp.com/";
+function getWeather(city, region) {
+	//const proxy = "https://cors-anywhere.herokuapp.com/";
 
-	const api = `${proxy}https://api.darksky.net/forecast/8c1a710cc5b01aaf9b78e7100381e1c4/${lat},${long}?exclude=minutely,hourly,flags`;
+	const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city},${region}?unitGroup=us&key=2VPLX39S6XKQKBP8FTT2G6A6G&include=fcst%2Chours%2Ccurrent`;
 
 	return fetch(api).then((res) => {
 		return res.json();
